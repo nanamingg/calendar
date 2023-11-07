245d85f19b8befe5a2f7165c1ed39746d07cba3d
nanamingg <nana172@kakao.com>
2023-11-07 17:40:21 +0900

    date change

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import DateData from "./date.json";
 const App = () => {
   return (
     <div className="min-h-screen flex flex-col justify-center items-center">
-      <h1 className="mb-12 text-2xl font-bold">2023년 11월</h1>
+      <h1 className="mb-12 text-2xl font-bol">2023년 11월</h1>
       <ul className="grid grid-cols-7 gap-4">
         <li className="w-32 h-32"></li>
         <li className="w-32 h-32"></li>
